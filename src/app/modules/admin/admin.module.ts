ff3067a6b59b70c8de38b4a9d3f80115cb76134a
esalazar24 <esalazar@unipacifico.edu.co>
2023-03-25 01:53:34 -0500

    funcional2

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { ConsultaSugerenciasComponent } from './consulta-sugerencias/consulta-sugerencias.component';
+import { PartesAnteproyectoComponent } from './partes-anteproyecto/partes-anteproyecto.component';
+import { RegistroAnteproyectoComponent } from './registro-anteproyecto/registro-anteproyecto.component';
+import { RegistroItemsComponent } from './registro-items/registro-items.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    ConsultaSugerenciasComponent,
+    PartesAnteproyectoComponent,
+    RegistroAnteproyectoComponent,
+    RegistroItemsComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,19 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 
-import { RegistroAnteproyectoComponent } from '../../registro-anteproyecto/registro-anteproyecto.component';
-import { PartesAnteproyectoComponent } from '../../partes-anteproyecto/partes-anteproyecto.component';
-import { RegistroItemsComponent } from '../../registro-items/registro-items.component';
-import { ConsultaSugerenciasComponent } from '../../consulta-sugerencias/consulta-sugerencias.component';
+
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'registro-anteproyecto', component: RegistroAnteproyectoComponent },
-  { path: 'partes-anteproyecto', component: PartesAnteproyectoComponent },
-  { path: 'registro-items', component: RegistroItemsComponent },
-  { path: 'consulta-sugerencias', component: ConsultaSugerenciasComponent },
+ 
 ];
 
 @NgModule({
